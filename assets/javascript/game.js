db5e272f3df299d4cabcc482d3d0e65067741c16
Jose Rivera <jose.alfredo.rivera@gmail.com>
2019-01-21 19:46:13 -0800

    Mobile Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,139 @@
-// Game Variables
-var wordList = [
-    "Keyboard",
-    "Monitor",
-    "Mouse",
-    "Bluetooth",
-    "Motherboard",
-    "USB",
-    "Processor",
-    "Memory",
-    "Ram",
-    "GPU"
-];
-var guessesAllowed = 6,
-    usedLettersList = [],
-    word = "",
-    letterCount = 0,
-    limeGreen = "#99f927";
-// Selectors
-var gameContainer = document.getElementById("GameContainer"),
-    banner = document.getElementById("Banner"),
-    gameDisplay = document.getElementById("GameDisplay"),
-    hangmanImg = document.getElementById("TheHangman"),
-    gameMsg = document.getElementById("GameMsg"),
-    winsDisplay = document.getElementById("WinsDisplay"),
-    currentWord = document.getElementById("CurrentWord"),
-    guessesDisplay = document.getElementById("GuessesDisplay"),
-    usedLettersDisplay = document.getElementById("UsedLettersDisplay"),
-    gameEndDisplay = document.getElementById("GameEndedDisplay"),
-    wordDisplay = document.getElementById("TheWord"),
-    gameEndMsg = document.getElementById("EndMsg"),
-    mobileKeyboard = document.getElementById('MobileKeyboard');
-
-// canvas
-var hangman = document.getElementById("HangmanCanvas");
-var ctx = hangman.getContext('2d');
-ctx.beginPath();
-ctx.strokeStyle = limeGreen;
-ctx.lineWidth = 2;
-
-// Desktop keypress
-function keypressed(e) {
-    // Get character
-    // var key = event.charCode || event.keyCode; // Get the Unicode value
-    // key = String.fromCharCode(key); // Convert the value into a character
-
-    // play game
-    play(e.key);
-}
-
-// Main
-function play(key) {
-    var guessCount = guessesDisplay.innerText; // Guesses left
-    // Update message after first key press
-    gameMsg.innerHTML = "";
-    // Game started
-    if (gameContainer.classList.contains("game-started")) {
-        // Check if key pressed is a letter
-        if (is_letter(key)) {
-            // Check if the letter has been used
-            // If letter hasn't been used play the game
-            var isUsedLetter = usedLettersList.includes(key);
-            if (!isUsedLetter) {
-                // If letter hasn't been used & player has not run out of guesses, play the game
-                if (guessCount > 0) {
-                    // Do stuff & update completed letters count
-                    letterCount = do_stuff(word, key, letterCount);
-                } else {
-                    // Game over
-                    setTimeout(function() { end_game("lost", word); }, 1000);
-                }
-                // Update used letters list
-                usedLettersList.push(key);
-            }
-        }
-    } else {
-        // Show main game display
-        gameDisplay.classList.remove("hidden");
-        // Select random word from list
-        word = wordList[Math.floor(Math.random() * wordList.length)];
-        // Setup game
-        game_setup(word);
+function game_init() {
+    game = {
+        wordList: [
+            'Windows',
+            'MacOs',
+            'Linux',
+            'Ubuntu',
+            'Keyboard',
+            'Monitor',
+            'Mouse',
+            'Bluetooth',
+            'Motherboard',
+            'USB',
+            'Processor',
+            'Memory',
+            'Ram',
+            'GPU',
+            'Fans',
+            'SSD',
+            'HDD',
+            'CPU'
+        ],
+        word: null,
+        letterCount: 0,
+        usedLettersList: [],
+        guessCount: 6,
+        wins: 0,
+        isMobile: false,
+        message: 'Press any key to get started!',
+        hasStarted: false,
+        hasEnded: false
     }
 }
 
-function game_setup(word) {
-    // console.log("The Word: " + word);
+// Selectors
+var gameContainer = document.getElementById('GameContainer'),
+    banner = document.getElementById('Banner'),
+    gameDisplay = document.getElementById('GameDisplay'),
+    hangmanImg = document.getElementById('TheHangman'),
+    gameMsg = document.getElementById('GameMsg'),
+    winsDisplay = document.getElementById('WinsDisplay'),
+    currentWord = document.getElementById('CurrentWord'),
+    guessesDisplay = document.getElementById('GuessesDisplay'),
+    usedLettersDisplay = document.getElementById('UsedLettersDisplay'),
+    gameEndDisplay = document.getElementById('GameEndedDisplay'),
+    wordDisplay = document.getElementById('TheWord'),
+    gameEndMsg = document.getElementById('EndMsg'),
+    mobileKeyboard = document.getElementById('MobileKeyboard');
+
+function play_game(key) {
+    // Game object not set
+    if (game.word == null) {
+        // Set game object
+        game.word = game.wordList[Math.floor(Math.random() * game.wordList.length)];
+        setup();
+        // console.log('Word: ' + game.word);
+    } else {
+        // Play
+        // console.log('Key: ' + key);
+        // Updates all on screen displays
+        if (game.hasStarted) {
+            // Check if key pressed is a letter
+            if (is_letter(key)) {
+                // Check if the letter has been used
+                // If letter hasn't been used play the game
+                var isUsedLetter = game.usedLettersList.includes(key);
+                if (!isUsedLetter) {
+                    // If letter hasn't been used & player has not run out of guesses, play the game
+                    if (game.guessCount > 0) {
+                        // Do stuff & update completed letters count
+                        var index = [],
+                            key = key.toLowerCase();
+                        game.word = game.word.toLowerCase();
+                        // Chec for multiple occurences
+                        for (var i = 0; i < game.word.length; i++) {
+                            var character = game.word.charAt(i);
+                            if (character == key) {
+                                index.push(i);
+                            }
+                        }
+                        // If character in word
+                        if (index.length > 0) {
+                            // if character in word more than once
+                            if (index.length > 1) {
+                                for (var x = 0; x < index.length; x++) {
+                                    update_current_word(index[x], key);
+                                    game.letterCount = game.letterCount + 1;
+                                }
+                            } else {
+                                update_current_word(index[0], key);
+                                game.letterCount = game.letterCount + 1;
+                            }
+                            update_used_letters(key);
+                            // If all letters completed end game w/ win
+                            if (game.letterCount == game.word.length) {
+                                end_game('Win');
+                            }
+                        } else {
+                            update_used_letters(key);
+                            // update_guesses_remaining();
+                            game.guessCount = game.guessCount - 1;
+                            if (game.guessCount === 0) {
+                                end_game('Lost');
+                            }
+                        }
+                    } else {
+                        // Game over
+                        setTimeout(function() { end_game('Lost'); }, 1000);
+                    }
+                    // Update used letters list
+                    game.usedLettersList.push(key);
+                }
+                // Update all displays
+                update();
+            }
+        }
+    }
+}
+
+function update() {
+    // fix to update all displays at end of each play
+    // Canvas
+    update_man(game.guessCount);
+    // Games Won
+    winsDisplay.textContent = game.wins;
+    // Guess Count / Remaining
+    guessesDisplay.textContent = game.guessCount;
+}
+
+function setup() {
+    // console.log('Word: ' + game.word);
+    if (gameContainer.classList.contains('is-mobile')) {
+        game.isMobile = true;
+    }
+    // fix to update all displays at end of each play
     // Set/Reset Game
-    usedLettersDisplay.innerHTML = '';
-    usedLettersList.length = 0;
-    letterCount = 0;
     // Reset canvas
     ctx.clearRect(0, 0, hangman.width, hangman.height);
     ctx.beginPath();
@@ -97,74 +142,44 @@ function game_setup(word) {
     // Get Saved Wins if they exist & set them
     var wins = sessionStorage.getItem('gameWins');
     if (isNaN(wins) || wins === null) {
-        wins = 0;
-        sessionStorage.setItem("gameWins", 0);
+        game.wins = 0;
+        sessionStorage.setItem('gameWins', 0);
+    } else {
+        game.wins = wins;
     }
-    winsDisplay.textContent = wins;
+    winsDisplay.textContent = game.wins;
     // Build underscores
     var underscores = '';
-    for (var i = 0; i < word.length; i++) {
+    for (var i = 0; i < game.word.length; i++) {
         underscores += '<span id="u-' + i + '">_</span>';
     }
     // Insert underscores
     currentWord.innerHTML = underscores;
-    guessesDisplay.textContent = guessesAllowed;
+    guessesDisplay.textContent = game.guessCount;
+    usedLettersDisplay.innerHTML = '';
     // Unhide display - Get Game Ready
-    if (gameDisplay.classList.contains("hidden")) {
-        gameDisplay.classList.remove("hidden");
+    if (gameDisplay.classList.contains('hidden')) {
+        gameDisplay.classList.remove('hidden');
     }
     // Mobile keyboard - build and display
-    if (gameContainer.classList.contains("is-mobile")) {
-        mobileKeyboard.classList.remove("hidden");
+    if (game.isMobile) {
+        reset_keyboard();
+        banner.classList.add('hidden');
+        mobileKeyboard.classList.remove('hidden');
     }
     // Hide end game display if shown
-    if (gameContainer.classList.contains("game-ended")) {
-        gameEndDisplay.classList.add("hidden");
-        gameContainer.classList.remove("game-ended");
+    if (game.hasEnded) {
+        gameEndDisplay.classList.add('hidden');
+        game.hasEnded = false;
     }
     // Add class to body
-    gameContainer.classList.add("game-started");
-}
-
-// For lack of a better name
-function do_stuff(word, letter, letterCount) {
-    var index = [],
-        letter = letter.toUpperCase(),
-        word = word.toUpperCase();
-    // Chec for multiple occurences
-    for (var i = 0; i < word.length; i++) {
-        var character = word.charAt(i);
-        if (character == letter) {
-            index.push(i);
-        }
-    }
-    // If character in word
-    if (index.length > 0) {
-        // if character in word more than once
-        if (index.length > 1) {
-            for (var x = 0; x < index.length; x++) {
-                update_current_word(index[x], letter);
-                letterCount = letterCount + 1;
-            }
-        } else {
-            update_current_word(index[0], letter);
-            letterCount = letterCount + 1;
-        }
-        update_used_letters(letter);
-    } else {
-        update_used_letters(letter);
-        update_guesses_remaining();
-    }
-    // If all letters completed end game w/ win
-    if (letterCount == word.length) {
-        end_game("Win", word);
-    }
-    return letterCount;
+    game.hasStarted = true;
+    gameMsg.textContent = '';
 }
 
 // Updates current word with correct letters
 function update_current_word(index, letter) {
-    document.getElementById("u-" + index).textContent = letter;
+    document.getElementById('u-' + index).textContent = letter;
 }
 
 // Updates used letters list
@@ -174,56 +189,62 @@ function update_used_letters(letter) {
     usedLettersDisplay.appendChild(usedLetter);
 }
 
-// Updates guesses remaining
-function update_guesses_remaining() {
-    var guessCount = guessesDisplay.textContent;
-    guessCount = parseInt(guessCount) - 1;
-    // If 0 guesses remaining update man & end game with loss
-    if (guessCount == 0) {
-        update_man(0);
-        setTimeout(function() { end_game("lost", word); }, 1000);
-    }
-    guessesDisplay.textContent = guessCount;
-    update_man(guessCount);
-}
-
 // Ends the game
 function end_game(status) {
-    gameContainer.classList.add("game-ended");
-    gameDisplay.classList.add("hidden");
-    if (gameContainer.classList.contains("is-mobile")) {
-        mobileKeyboard.classList.add("hidden");
-        reset_keyboard();
+    game.hasEnded = true;
+    gameDisplay.classList.add('hidden');
+    if (game.isMobile) {
+        mobileKeyboard.classList.add('hidden');
+        // reset_keyboard();
     }
     // Sounds from Retro.sx
     // Update display with game outcome and play sound at game end
-    var sound = "";
-    if (status == "Win") {
-        var gamesWon = parseInt(sessionStorage.getItem("gameWins"));
-        gamesWon = gamesWon + 1;
-        sessionStorage.setItem("gameWins", gamesWon);
-        gameEndMsg.textContent = "YOU WIN!";
-        sound = "./assets/sounds/win.mp3";
+    var sound = '';
+    if (status == 'Win') {
+        var gamesWon = parseInt(sessionStorage.getItem('gameWins'));
+        game.wins = gamesWon + 1;
+        sessionStorage.setItem('gameWins', game.wins);
+        gameEndMsg.textContent = 'YOU WIN!';
+        sound = './assets/sounds/win.mp3';
     } else {
-        gameEndMsg.textContent = "GAME OVER!";
-        sound = "./assets/sounds/player-down.mp3";
+        gameEndMsg.textContent = 'GAME OVER!';
+        sound = './assets/sounds/player-down.mp3';
     }
     var audio = new Audio(sound);
     audio.play();
     // Display word
-    wordDisplay.textContent = word;
+    wordDisplay.textContent = game.word;
     // Update game message for next game
-    gameMsg.textContent = "Press any key to get started!";
-    gameEndDisplay.classList.remove("hidden");
-    gameContainer.classList.remove("game-started");
+    gameMsg.textContent = game.message;
+    banner.classList.remove('hidden');
+    gameEndDisplay.classList.remove('hidden');
+    game.hasStarted = false;
+    // Reset Game
+    reset_game();
 }
 
-// Returns true if key is a letter
+// Resets Game Object
+function reset_game() {
+    game.word = null;
+    game.letterCount = 0;
+    game.usedLettersList.length = 0;
+    game.guessCount = 6;
+}
+
+// Helper Methods
 function is_letter(key) {
     var objRegExp = /^[a-z]+$/;
     return objRegExp.test(key);
 }
 
+// HTML Canvas Methods
+var limeGreen = '#99f927',
+    hangman = document.getElementById('HangmanCanvas'),
+    ctx = hangman.getContext('2d');
+
+ctx.beginPath();
+ctx.strokeStyle = limeGreen;
+ctx.lineWidth = 2;
 // Draws the hangman canvas
 function draw(fromX, fromY, toX, toY) {
     ctx.beginPath();
@@ -243,7 +264,7 @@ function frame() {
     draw(10, 390, 290, 390);
     ctx.setLineDash([]);
 }
-// body parts
+// Body parts
 function head() {
     draw(150, 20, 150, 40);
     // circle
@@ -276,34 +297,54 @@ var bodyPart = [rightLeg, leftLeg, rightArm, leftArm, torso, head];
 
 // Updates the hangman
 function update_man(n) {
-    bodyPart[n]();
+    if (n !== 6) {
+        bodyPart[n]();
+    }
 }
 
-// Mobile Keyboard --->
-var keyRows = [
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-];
+// Desktop Methods
+// Detects keyboard keypress
+// Locks keyboard after keypress to prevent fast keypress
+var locked = false;
 
+function keypressed(e) {
+    console.log('Locked: ' + locked);
+    if (!locked) {
+        locked = true;
+        play_game(e.key);
+    }
+    setTimeout(function() { locked = false; }, 200);
+}
+
+// Mobile Methods
 // Builds the mobile keyboard
 function build_keyboard() {
+    var keyRows = [
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+        ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+    ];
     for (var i = 0; i < keyRows.length; i++) {
         var keyboardKeys = document.createElement('ul');
         keyboardKeys.id = 'KeyboardRow-' + i;
         for (var x = 0; x < keyRows[i].length; x++) {
             letterKey = document.createElement('li');
             letterKey.id = 'letter-' + keyRows[i][x];
+            letterKey.classList.add('letters');
             letterKey.innerHTML = keyRows[i][x];
             mobileKeyboard.appendChild(keyboardKeys);
             keyboardKeys.appendChild(letterKey);
         }
     }
+    var letters = document.querySelectorAll('li');
+    for (var i = 0; i < letters.length; i++) {
+        letters[i].classList.remove('key-disabled');
+    }
 }
 
 // Resets the mobile keyboard
 function reset_keyboard() {
-    mobileKeyboard.innerHTML = "";
+    mobileKeyboard.innerHTML = '';
     build_keyboard();
 }
 
@@ -319,9 +360,9 @@ function mobile_keypressed() {
     mobileKeyboard.onclick = function(event) {
         var target = getEventTarget(event);
         key = target.innerHTML;
-        play(key);
+        play_game(key);
         // Disable key
-        var pressedKey = document.getElementById("letter-" + key);
+        var pressedKey = document.getElementById('letter-' + key);
         pressedKey.classList.add('key-disabled');
     }
 }
@@ -329,8 +370,8 @@ function mobile_keypressed() {
 // Mobile start
 function mobile_start() {
     banner.onclick = function(event) {
-        mobileKeyboard.classList.remove("hidden");
-        play("");
+        mobileKeyboard.classList.remove('hidden');
+        play_game('');
     }
 }
 
@@ -342,15 +383,17 @@ function mobile_check() {
         b = d.getElementsByTagName('body')[0],
         width = w.innerWidth || e.clientWidth || g.clientWidth,
         height = w.innerHeight || e.clientHeight || g.clientHeight;
-    console.log("w.innerWidth || e.clientWidth || g.clientWidth" + width);
+    // console.log('w.innerWidth || e.clientWidth || g.clientWidth' + width);
     // if mobile run mobile functions
     if (width <= 768) {
-        gameContainer.classList.add("is-mobile");
+        console.log('Is Mobile');
+        gameContainer.classList.add('is-mobile');
         mobile_start();
-        build_keyboard();
         mobile_keypressed();
     }
 }
 
 // Check if mobile
 mobile_check();
+
+game_init();
